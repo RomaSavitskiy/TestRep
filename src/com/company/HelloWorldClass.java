5f43103cc0222f2de2cc3714df784dc495a975a0
RomanSavitskiy <roma.savitskiy@inbox.ru>
2022-07-03 11:36:51 +0300

    Added Vitalya message

--- src/com/company/HelloWorldClass.java
+++ src/com/company/HelloWorldClass.java
@@ -4,4 +4,8 @@ public class HelloWorldClass {
     public static void main(String[] args){
         System.out.println("Hello World");
     }
+
+    public static void Vitalya(){
+        System.out.println("Bedniy Vitalya");
+    }
 }
